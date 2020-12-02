9f6ed9dde073657136d86aa917b32b102f679a28
Nikolaj Witting <njw@trackman.com>
2020-12-02 07:25:28 +0100

    day2

--- main.py
+++ main.py
@@ -23,12 +23,43 @@ def day1b():
                     print("cost1: " + str(cost) + " cost2: " + str(cost2) + " cost3: " + str(cost3))
                     print(cost*cost2*cost3)
 
+def day2():
+    text_file = open("day2.txt", "r")
+    lines = text_file.readlines()
 
+    lines2 = [i.split() for i in lines]
+    validcount=0
+    for line in lines2:
+        limit=line[0].split("-")
+        min=int(limit[0])
+        max=int(limit[1])
+        char=line[1][0]
+        password=line[2]
+        if password.count(char)>=min and password.count(char)<=max:
+            validcount+=1
+            print(line)
+    print("Number of valid passwords: "+str(validcount))
 
+def day2b():
+    text_file = open("day2.txt", "r")
+    lines = text_file.readlines()
 
-# Press the green button in the gutter to run the script.
+    lines2 = [i.split() for i in lines]
+    validcount=0
+    for line in lines2:
+        limit=line[0].split("-")
+        min=int(limit[0])
+        max=int(limit[1])
+        char=line[1][0]
+        password=line[2]
+        if bool(password[min-1]==char) != bool(password[max-1]==char):
+            validcount+=1
+            print(line)
+    print("Number of valid passwords: "+str(validcount))
+
 if __name__ == '__main__':
-    day1()
-    day1b()
+    #day1()
+    #day1b()
+    #day2()
+    day2b()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
